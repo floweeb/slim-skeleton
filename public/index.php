bae5b484d034c1c296b4179cfb0ece13dd13e420
floweeb <floydqaranja@gmail.com>
2024-10-17 19:18:08 +0300

    feat: added error middleware

--- public/index.php
+++ public/index.php
@@ -12,15 +12,21 @@ define('APP_ROOT', dirname(__DIR__));
 require APP_ROOT . '/vendor/autoload.php';
 
 $builder = new ContainerBuilder;
+// Adding dB connection details for container to use for starting up class.
 $container = $builder->addDefinitions(APP_ROOT . '/config/database_conn.php')
     ->build();
 AppFactory::setContainer($container);
 
 $app = AppFactory::create();
 
+$error_middleware = $app->addErrorMiddleware(true, true, true); //set to false, the first one I think, in prod.
+$error_middleware->getDefaultErrorHandler()->forceContentType('application/json');
+
+
 $app->get('/{name}', function (Request $request, Response $response, array $args) {
-    $pdo = $this->get(App\Database::class);
-    $pdo->getConnection();
+    // Use this to test database connection before continuing
+    $this->get(App\Database::class)->getConnection();
+
     $name = $args['name'];
     $response->getBody()->write("Hello, $name");
     return $response;
