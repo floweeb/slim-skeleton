4db38f334684dd16083e7626f3c47cf2a68e7f7f
floweeb <floydqaranja@gmail.com>
2024-10-17 13:41:17 +0300

    feat: Added a basic PSR-4 structure to the project and a database.php file for the database connection, only mysql, want a postgres one too.

---
+++ src/App/Database.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use PDO;
+
+class Database
+{
+    public function __construct(
+        private string $host,
+        private string $db_name,
+        private string $user,
+        private string $password,
+    ) {}
+    public function getConnection(): PDO
+    {
+        $dsn = "mysql:host=$this->host;dbname=$this->db_name;charset=utf8";
+        return new PDO($dsn, $this->user, $this->password, [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]);
+    }
+}
